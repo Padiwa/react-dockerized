f911965ad7b625b47835ffc3a402821f5e21dabd
cmsassot <cmsassot@cadoles.com>
2022-10-26 18:38:54 +0200

    Passage tache en fonction

--- src/components/Tache.jsx
+++ src/components/Tache.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Tache extends Component {
-    render() {
-        // const details = this.props.details;
-        // const onDelete = this.props.onDelete;
-        //destructuring :
-        const { details, onDelete } = this.props;
-        return (
-            <li key={details.id}>
-                {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
-            </li>
-        )
-    }
+function Tache({ details, onDelete }) {
+    // const details = this.props.details;
+    // const onDelete = this.props.onDelete;
+    //destructuring :
+    return (
+        <li key={details.id}>
+            {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
+        </li>
+    )
 }
 export default Tache;
