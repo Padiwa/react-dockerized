19fe51d55afef11a09f6c209e511eb38c2bc896d
cmsassot <cmsassot@cadoles.com>
2022-10-26 18:40:34 +0200

    Tache 'functional component'

--- src/components/Tache.jsx
+++ src/components/Tache.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 
-function Tache({ details, onDelete }) {
-    // const details = this.props.details;
-    // const onDelete = this.props.onDelete;
-    //destructuring :
-    return (
-        <li key={details.id}>
-            {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
-        </li>
-    )
-}
+export const Tache = ({ details, onDelete }) => (
+    <li key={details.id}>
+        {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
+    </li>
+);
 export default Tache;
