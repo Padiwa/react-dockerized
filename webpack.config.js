6cbb8e067827c64f52dd1b873fdd4d702620105a
cmsassot <cmsassot@cadoles.com>
2022-10-28 17:56:07 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+    path:path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, "src", "index.html"),
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
-    },
-    port: 3000,
-  },
-  module: {
-    // exclude node_modules
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: ["css-loader"],
-      }
-    ],
-  },
   // pass all js files through Babel
   resolve: {
     extensions: ["*", ".js", ".jsx"],
   },
-};
+}
