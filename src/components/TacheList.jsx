fba8fee2f45a36cbce04084835ef6609776c7873
cmsassot <cmsassot@cadoles.com>
2022-10-30 15:39:37 +0100

    Enregistrement en localStorage

--- src/components/TacheList.jsx
+++ src/components/TacheList.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import Tache from "./Tache";
 const TacheList = () => {
     const taches = useSelector((state) => state.todo);
-
     return (
         <ul className="m-6">
             {taches.map(tache => (
--- src/components/redux.js
+++ src/components/redux.js
@@ -1,9 +1,12 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit"
+const loadState = () => {
+     const saved = localStorage.getItem("todo");
+     const initialValue = JSON.parse(saved);
+     return initialValue ? initialValue : [{id:1,text:'test',done:false}];
+ }
 const todoslice = createSlice({
     name:"todo",
-    initialState: [
-        {id:1,text:'test',done:false}
-    ],
+    initialState: loadState(),
     reducers: {
         addTask: (state, actions) => {
             const newTask = {
@@ -12,21 +15,28 @@ const todoslice = createSlice({
                 text: actions.payload
             }
             state.push(newTask)
+            localStorage.removeItem("todo");
+            localStorage.setItem("todo", JSON.stringify(state));
+            
         },
         toggleTask: (state, action) => {
             const task = state.find(t => t.id === action.payload);
             task.done = !task.done;
-            
+            localStorage.removeItem("todo");
+            localStorage.setItem("todo", JSON.stringify(state));
+
         },
         deleteTask: (state, action) => {
             state = state.filter(t => t.id !== action.payload);
+            localStorage.removeItem("todo");
+            localStorage.setItem("todo", JSON.stringify(state));
+
             return state;
         }
     }
 })
 
 export const { addTask, deleteTask, toggleTask} = todoslice.actions
-
 export const store = configureStore({
     reducer: {
         todo: todoslice.reducer
